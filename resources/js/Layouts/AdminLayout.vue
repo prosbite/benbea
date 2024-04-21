8021a54e0976f3b4aa9d994dff7398f3e32da6c8
Prosbite <brainsnap.2012@gmail.com>
2024-04-21 16:46:37 +0800

    layout admin

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+import NavLink from '@/Components/NavLink.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <div class="h-16 w-full bg-red-500 flex justify-center">
+            <div class="flex bg-white w-full h-full max-w-[1300px] justify-between">
+                <div class="logo flex items-center">
+                    <NavLink :href="route('dashboard')">
+                        <span class="text-2xl font-bold text-blue-500">Ben&amp;</span>
+                        <span class="text-2xl text-pink-500">Bea</span>
+                    </NavLink>
+                </div>
+            </div>
+        </div>
+        <div class="flex max-w-[1300px] min-h-screen flex-1">
+            <!-- sidebar -->
+            <div class="flex flex-col bg-white w-48">
+
+            </div>
+            <div class="flex flex-1 bg-gray-50">
+                <slot />
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/admin/Dashboard.vue
+++ resources/js/Pages/admin/Dashboard.vue
@@ -1,8 +1,14 @@
-<template>
-    <div>
-        Hello World
-    </div>
-</template>
-<script setup lang="ts">
-
+<script setup>
+import { Head } from '@inertiajs/vue3'
+import AdminLayout from '@/Layouts/AdminLayout.vue'
+import NavLink from '@/Components/NavLink.vue'
 </script>
+
+<template>
+    <Head title="Dashboard" />
+    <AdminLayout>
+        <div class="flex">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere animi aliquam blanditiis eius unde quis suscipit, assumenda nam officia dolor nihil odit ea explicabo repellat voluptates esse, quo laboriosam amet?
+        </div>
+    </AdminLayout>
+</template>
